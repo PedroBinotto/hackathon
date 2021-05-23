4fb7c06ca3b6916dd404cf8dd3650fbec7fb1091
Pedro Binotto <pedrosantibinotto@gmail.com>
2021-05-23 18:11:37 -0300

    telas

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -3,12 +3,52 @@ from flask import (Blueprint, abort, flash, redirect, render_template, request,
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/', methods=['GET'])
+def index():
+	return render_template('index.html')
+
+
+@main.route('/login', methods=['GET'])
+def login():
+	return render_template('login.html')
+
+
 @main.route('/home', methods=['GET'])
 def home():
 	return render_template('home.html')
 
 
-@main.route('/ong', methods=['GET'])
+@main.route('/ong/branch', methods=['GET'])
 def ong():
+	pass
+
+
+@main.route('/ong/select', methods=['GET'])
+def ong_select():
+	pass
+
+
+@main.route('/ong/select/service', methods=['GET'])
+def ong_service():
+	pass
+
+
+@main.route('/ong/professionals', methods=['GET'])
+def ong_professionals():
+	pass
+
+
+@main.route('/ong/profile', methods=['GET'])
+def ong_profile():
+	pass
+
+
+@main.route('/professional/profile', methods=['GET'])
+def prof_profile():
+	pass
+
+
+@main.route('/professional/ongs', methods=['GET'])
+def prof_ongs():
 	pass
